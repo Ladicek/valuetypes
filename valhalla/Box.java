a69491b488e8c30200439bbea7c5f32cd4efe346
Ladislav Thon <ladicek@gmail.com>
2016-02-06 11:10:15 +0100

    modify Valhalla examples to show some more interesting info

--- valhalla/Box.java
+++ valhalla/Box.java
@@ -11,7 +11,14 @@ public final class Box<any T> {
   }
 
   public static void main(String[] args) {
-    System.out.println(new Box<int>(1));
-    System.out.println(new Box<Integer>(1));
+    Box<int> b1 = new Box<int>(1);
+    System.out.println(b1);
+
+    Box<Integer> b2 = new Box<Integer>(1);
+    System.out.println(b2);
+
+    Box<int> b3 = new Box<>(1);
+    Box<Integer> b4 = new Box<>(new Integer(1));
+    //Box<Integer> b5 = new Box<>(1);
   }
 }
--- valhalla/Lists.java
+++ valhalla/Lists.java
@@ -1,5 +1,6 @@
 import java.anyutil.List;
 import java.anyutil.ArrayList;
+import java.anyutil.Arrays;
 
 public class Lists {
   public static void main(String[] args) {
@@ -8,7 +9,35 @@ public class Lists {
     l1.add(1); l2.add(1);
     l1.add(2); l2.add(2);
     l1.add(3); l2.add(3);
+
     System.out.println(l1);
+    printClass(l1.getClass());
+
+    System.out.println();
+
     System.out.println(l2);
+    printClass(l2.getClass());
+  }
+
+  private static void printClass(Class clazz) {
+    Class c = clazz;
+    while (c != null) {
+      System.out.println(c);
+      printInterfaces(c, 1);
+      c = c.getSuperclass();
+    }
+  }
+
+  private static void printInterfaces(Class clazz, int nestLevel) {
+    StringBuilder b = new StringBuilder();
+    for (int i = 0; i < nestLevel; i++) {
+      b.append("  ");
+    }
+    String indent = b.toString();
+
+    for (Class iface : clazz.getInterfaces()) {
+      System.out.println(indent + iface);
+      printInterfaces(iface, nestLevel + 1);
+    }
   }
 }
